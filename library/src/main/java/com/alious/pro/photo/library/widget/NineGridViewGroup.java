58a46544041129a15ee4eeaf10b86eb179e9db30
aliouswang <wsw622@126.com>
2016-09-05 18:13:07 +0800

    init commit

--- library/src/main/java/com/alious/pro/photo/library/widget/NineGridViewGroup.java
+++ library/src/main/java/com/alious/pro/photo/library/widget/NineGridViewGroup.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import com.alious.pro.photo.library.R;
 import com.alious.pro.photo.library.interfaces.INineGridAdapter;
 
+import java.util.ArrayList;
+
 /**
  * Nine grid view group,
  *
@@ -16,9 +18,11 @@ import com.alious.pro.photo.library.interfaces.INineGridAdapter;
 public class NineGridViewGroup extends ViewGroup{
 
     public static final int DEFAULT_MAX_SIZE = 9;
+    public static final int DEFAULT_COLUMN_SIZE = 3;
     public static final float DEFAULT_RATIO = 1.0f;
 
     private int mMaxSize;
+    private int mColumnSize;
     private int mHorizontalGap;
     private int mVerticalGap;
     private float mRatio;
@@ -26,6 +30,8 @@ public class NineGridViewGroup extends ViewGroup{
     private int mCellSize;
     private int mRowCount;
     private int mColumnCount;
+
+    private ArrayList<Point> mPoints;
 
     private INineGridAdapter mGridAdapter;
 
@@ -44,6 +50,8 @@ public class NineGridViewGroup extends ViewGroup{
                 R.styleable.NineGridViewGroup);
         this.mMaxSize = typedArray.getInt(R.styleable.NineGridViewGroup_maxSize,
                 DEFAULT_MAX_SIZE);
+        this.mColumnSize = typedArray.getInt(R.styleable.NineGridViewGroup_columnSize,
+                DEFAULT_COLUMN_SIZE);
         this.mHorizontalGap = typedArray.getDimensionPixelSize(R.styleable.NineGridViewGroup_horizontal_gap,
                 getResources().getDimensionPixelSize(R.dimen.default_horizontal_gap));
         this.mVerticalGap = typedArray.getDimensionPixelSize(R.styleable.NineGridViewGroup_vertical_gap,
@@ -55,7 +63,29 @@ public class NineGridViewGroup extends ViewGroup{
 
     public void setGridAdapter(INineGridAdapter gridAdapter) {
         mGridAdapter = gridAdapter;
+        calculateCellPoint();
         requestLayout();
+    }
+
+    private void calculateCellPoint() {
+        int count = getCount();
+        if (count > 0) {
+            mPoints = new ArrayList<>(count);
+            for (int i = 0; i < count; i++) {
+                Point point = new Point();
+                point.row = i % mColumnSize;
+                point.column = i / mColumnSize;
+                mPoints.add(point);
+            }
+            mColumnCount = mColumnSize;
+            mRowCount = (int) Math.ceil(count / mColumnSize);
+        }else {
+            mPoints = new ArrayList<>();
+        }
+    }
+
+    private Point getPointByPosition(int pos) {
+        return mPoints.get(pos);
     }
 
     /**
@@ -85,12 +115,22 @@ public class NineGridViewGroup extends ViewGroup{
             if (isSingle()) {
                 mCellSize = totalSpace;
             }else {
+                mCellSize = (totalSpace - mHorizontalGap * (mColumnSize - 1)) / mColumnSize;
             }
+            float mCellHeight = mCellSize * mRatio;
+            float totalHeight = mCellHeight * mRowCount + getPaddingTop() + getPaddingBottom()
+                    + (mRowCount - 1) * mVerticalGap;
+            setMeasuredDimension(sWidth, (int)totalHeight);
         }
     }
 
     @Override
     protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
 
+    }
+
+    private class Point {
+        int row;
+        int column;
     }
 }
