1083fb0f1d69c9881f55d6d54175db950190dd43
aliouswang <wsw622@126.com>
2016-09-07 18:02:06 +0800

    do refactor

--- library/src/main/java/com/alious/pro/photo/library/adapter/FrescoPhotoPageAdapter.java
+++ library/src/main/java/com/alious/pro/photo/library/adapter/FrescoPhotoPageAdapter.java
@@ -1,8 +1,5 @@
 package com.alious.pro.photo.library.adapter;
 
-import android.graphics.drawable.Animatable;
-import android.net.Uri;
-import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +7,8 @@ import android.view.ViewGroup;
 
 import com.alious.pro.photo.library.R;
 import com.alious.pro.photo.library.interfaces.NineImageUrl;
+import com.alious.pro.photo.library.utils.ImageLoadUtil;
 import com.alious.pro.photo.library.widget.RatioSimpleDraweeView;
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.controller.BaseControllerListener;
-import com.facebook.drawee.controller.ControllerListener;
-import com.facebook.drawee.interfaces.DraweeController;
-import com.facebook.imagepipeline.image.ImageInfo;
 
 import java.util.List;
 
@@ -41,35 +34,7 @@ public class FrescoPhotoPageAdapter extends PagerAdapter{
                 .inflate(R.layout.view_pager_preview, null);
         final RatioSimpleDraweeView img_head = (RatioSimpleDraweeView) rootView.findViewById(R.id.img_head);
         container.addView(rootView);
-//        ImageLoadUtil.loadScaleWithFresco(imageView, mPreviewImages.get(position));
-        ControllerListener controllerListener = new BaseControllerListener<ImageInfo>() {
-            @Override
-            public void onFinalImageSet(
-                    String id,
-                    @Nullable ImageInfo imageInfo,
-                    @Nullable Animatable anim) {
-                if (imageInfo == null) {
-                    return;
-                }
-                float mScale = (float) imageInfo.getHeight() / (float) imageInfo.getWidth();
-                img_head.setRatio(mScale);
-            }
-
-            @Override
-            public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
-            }
-
-            @Override
-            public void onFailure(String id, Throwable throwable) {
-            }
-        };
-        Uri uri = Uri.parse(mPreviewImages.get(position).getNineImageUrl());
-        DraweeController controller = Fresco.newDraweeControllerBuilder()
-                .setControllerListener(controllerListener)
-                .setUri(uri)
-                .build();
-        img_head.setController(controller);
-
+        ImageLoadUtil.loadScaleWithFresco(img_head, mPreviewImages.get(position).getNineImageUrl());
         return rootView;
     }
 
