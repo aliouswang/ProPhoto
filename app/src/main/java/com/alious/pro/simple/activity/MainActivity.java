1177a8980308de71e8fb39e0062f27c256618103
aliouswang <wsw622@126.com>
2016-09-10 22:46:50 +0800

    add photo view

--- app/src/main/java/com/alious/pro/simple/activity/MainActivity.java
+++ app/src/main/java/com/alious/pro/simple/activity/MainActivity.java
@@ -6,17 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.alious.pro.photo.library.widget.NineFrescoGridViewGroup;
-import com.alious.pro.photo.library.widget.NinePicassoGridViewGroup;
-import com.alious.pro.photo.library.widget.RatioImageView;
 import com.alious.pro.simple.R;
 
 public class MainActivity extends AppCompatActivity {
-
-    private RatioImageView img_head;
-
-    private NinePicassoGridViewGroup nine_grid;
-    private NineFrescoGridViewGroup nine_grid2;
 
     private Button btn_goto_fresco;
     private Button btn_goto_picasso;
@@ -31,35 +23,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-//        img_head = (RatioImageView) findViewById(R.id.img_head);
-//
-//        Picasso.with(MainActivity.this)
-//                .load(Photo.images[7])
-//                .into(img_head);
-//
-//        final ArrayList<Data> datas = new ArrayList<>();
-//        for (String str : Photo.images) {
-//            Data data = new Data();
-//            data.setImage_url(str);
-//            datas.add(data);
-//        }
-//
-//        img_head.setOnClickListener(new View.OnClickListener() {
-//                                        @Override
-//                                        public void onClick(View view) {
-//                                        }
-//                                    });
-
-//        DefaultNineImageAdapter<Data> defaultNineImageAdapter =
-//                new DefaultNineImageAdapter(datas);
-//        nine_grid = (NinePicassoGridViewGroup) findViewById(R.id.nine_grid);
-//        nine_grid.setGridAdapter(defaultNineImageAdapter);
-//
-//        DefaultNineImageAdapter<Data> defaultNineImageAdapter2 =
-//                new DefaultNineImageAdapter(datas);
-//        nine_grid2 = (NineFrescoGridViewGroup) findViewById(R.id.nine_grid2);
-//        nine_grid2.setGridAdapter(defaultNineImageAdapter2);
-//
         btn_goto_fresco = (Button) findViewById(R.id.btn_goto_fresco);
         btn_goto_fresco.setOnClickListener(new View.OnClickListener(){
 
